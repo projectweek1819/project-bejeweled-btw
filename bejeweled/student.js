cc09cca785257e738233381d64000fcd3d2bad80
Niels Bergmans <niels.bergmans@student.ucll.be>
2018-12-18 13:36:02 +0100

    finished exercises

--- bejeweled/student.js
+++ bejeweled/student.js
@@ -34,7 +34,7 @@ function horizontalChainAt(grid, position) {
     let tx = position.x + 1;
 
     while (true) {
-        if (isInside(grid,{x: tx, y: ty}) && grid[ty][tx] === color) {
+        if (isInside(grid, {x: tx, y: ty}) && grid[ty][tx] === color) {
             right++;
             tx++;
         } else {
@@ -46,7 +46,7 @@ function horizontalChainAt(grid, position) {
     ty = position.y;
     tx = position.x - 1;
     while (true) {
-        if (isInside(grid,{x: tx, y: ty}) && grid[ty][tx] === color) {
+        if (isInside(grid, {x: tx, y: ty}) && grid[ty][tx] === color) {
             left++;
             tx--;
         } else {
@@ -54,19 +54,19 @@ function horizontalChainAt(grid, position) {
         }
     }
 
-    return [left + right + 1,left,right];
+    return left + right + 1;
 }
 
-function verticalChainAt(grid,position){
+function verticalChainAt(grid, position) {
     let color = grid[position.y][position.x];
     let up = 0;
     let down = 0;
 
-    let ty = position.y-1;
+    let ty = position.y - 1;
     let tx = position.x;
 
     while (true) {
-        if (isInside(grid,{x: tx, y: ty}) && grid[ty][tx] === color) {
+        if (isInside(grid, {x: tx, y: ty}) && grid[ty][tx] === color) {
             up++;
             ty--;
         } else {
@@ -75,40 +75,76 @@ function verticalChainAt(grid,position){
     }
 
 
-    ty = position.y+1;
+    ty = position.y + 1;
     tx = position.x;
     while (true) {
-        if (isInside(grid,{x: tx, y: ty}) && grid[ty][tx] === color) {
+        if (isInside(grid, {x: tx, y: ty}) && grid[ty][tx] === color) {
             down++;
             ty++;
         } else {
             break;
         }
     }
-    return [up + down + 1,up,down];
+    return up + down + 1;
 }
 
-function removeChains(grid){
 
-    let maxX = width(grid);
-    let maxY = width(grid);
-    let result =
-    for (let x = 0; x < maxX; x++){
-        for (let y = 0; y < maxY; y++){
-            if (horizontalChainAt({x:x,y:y})[0] >= 3){
-                let left = horizontalChainAt({x:x,y:y})[1];
-                let right = horizontalChainAt({x:x,y:y})[2];
-                grid[y][x] = "";
-                while (left > 0){
-                    grid[y][x-left] = "";
-                    left--;
-                }
-                while (right > 0){
-                    gird[y][x+right] = "";
-                    right--;
+function removeChains(grid) {
+    let toRemove = [];
+    let colorCount = [0, 0, 0];
+    let colors = ["red", "green", "blue"];
+    for (let x = 0; x < width(grid); x++) {
+        for (let y = 0; y < height(grid); y++) {
+            if (horizontalChainAt(grid, {x: x, y: y}) >= 3) {
+                let color = grid[y][x];
+                for (let i = 0; i < colors.length; i++) {
+                    if (color === colors[i]) {
+                        colorCount[i] += 1;
+                        toRemove.push({x: x, y: y})
+                    }
                 }
             }
-            if if (horizontalChainAt({x:x,y:y})[0] >= 3){
+            if (verticalChainAt(grid, {x: x, y: y}) >= 3) {
+                let color = grid[y][x];
+                for (let i = 0; i < colors.length; i++) {
+                    if (color === colors[i]) {
+                        colorCount[i] += 1;
+                        toRemove.push({x: x, y: y})
+                    }
+                }
+            }
+        }
+    }
+    for (let i = 0; i < toRemove.length; i++) {
+        grid[toRemove[i].y][toRemove[i].x] = "";
+    }
+
+    let result = {};
+    if (colorCount[0] > 0) {
+        result.red = colorCount[0];
+    }
+    if (colorCount[1] > 0) {
+        result.green = colorCount[1];
+    }
+    if (colorCount[2] > 0) {
+        result.blue = colorCount[2];
+    }
+
+    return result;
+
+}
+
+function collapse(grid) {
+    for (let x = 0; x < width(grid); x++) {
+        for (let y = 0; y < height(grid); y++) {
+            if (grid[y][x] === ""){
+                let ty = y;
+                while (ty > 0){
+                    grid[ty][x] = grid[ty-1][x];
+                    ty--;
+                }
+                grid[0][x] = "";
+            }
         }
     }
 }
